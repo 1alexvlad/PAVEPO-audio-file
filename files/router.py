c489cad6c3503b60340e9c0771d41e4f4378b4af
Vladislav Alexandrov <texr44760@gmail.com>
2025-03-28 22:18:21 +0300

    добавить имя и сохранить файл

---
+++ files/router.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, File, HTTPException, UploadFile
+
+
+router_file = APIRouter(
+    prefix='/files'
+)
+
+@router_file.post('/')
+async def upload_file(name_file: str, uploaded_file: UploadFile = File(...)):
+    file = uploaded_file.file
+    filename = uploaded_file.filename
+
+    if not filename.endswith('.mp3'):
+        raise HTTPException(status_code=400, detail='Неверный формат файла, передайте, пожалуйста, аудио формат - .mp3') 
+
+    with open(f"name_file", 'wb') as f:
+        f.write(file.read())
+        return {'message': 'Данные успешно сохранились'}
